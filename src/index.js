11d09c29581ae59b86d669b338af778786acfbc0
Abrafaks <abrafakspl@gmail.com>
2021-07-04 13:41:13 +0200

    xd

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-const host = "127.0.0.1";
-app.listen(port, host, () => {
+
+app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
